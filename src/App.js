8652f1a37ee942e7e58e80b0695f5388ff90823e
chris <christian.sven.erik.myren@gmail.com>
2024-04-15 18:58:51 +0200

    - Fix the styling of App.js and NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import "./App.css";
 
 import NavBar from "./components/NavBar";
-import Header from "./components/Header";
 import { menuData } from "./resources/data";
 
 function App() {
+  const logo = "logo.png";
+
   return (
-    <div className="App">
-      <NavBar />
-      <Header />
+    <div
+      className="App"
+      style={{
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+        top: 0,
+        bottom: 0,
+        margin: 0,
+        padding: 0,
+      }}
+    >
+      <NavBar logo={logo} />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,46 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Col, Container, Navbar, Row, Nav } from "react-bootstrap";
+import { List } from "react-bootstrap-icons";
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const { logo } = props;
+
   return (
-    <Navbar>
-      <Container>
-        <h4>Hey, look at my navbar!</h4>
+    <Navbar expand="xl" style={{ paddingTop: "0px" }}>
+      <Container
+        fluid
+        style={{
+          paddingLeft: "0px",
+          paddingRight: "0px",
+        }}
+      >
+        <Row
+          style={{
+            backgroundColor: "green",
+            color: "white",
+            width: "100%",
+            marginLeft: "0px",
+          }}
+          className="pt-2 pb-3"
+        >
+          <Col sm={1}>
+            <a className="navbar-brand" href="#Home">
+              <img
+                src={logo}
+                width={75}
+                height={65}
+                alt={logo}
+                className="pt-1 pb-1"
+              />
+            </a>
+          </Col>
+          <Col sm={10} className="text-center align-content-center">
+            <h1>EatEatEat</h1>
+          </Col>
+          <Col sm={1} className="text-end align-content-center">
+            <List size={30} />
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
